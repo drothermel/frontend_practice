1f3e538ba8c5f542203e5168f2f199ff77697e82
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-11 17:36:52 -0500

    converted MorselCard tags into badges

--- snippet_viewer/src/lib/components/MorselCard.svelte
+++ snippet_viewer/src/lib/components/MorselCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Badge } from "$lib/components/ui/badge";
   import { type Morsel } from "$lib/types/types";
   let { morsel }: { morsel: Morsel } = $props();
 </script>
@@ -6,10 +7,13 @@
 <div class="flex flex-col border border-green-800 gap-4 p-4">
   <h3>{morsel.title}</h3>
   <p>Created: {morsel.time.toLocaleString()}</p>
-  {#if morsel.tags.length > 0}
-    <p>Tags: {morsel.tags.join(", ")}</p>
-  {:else}
-    <p>No Tags</p>
+  {#if morsel.tags.length > 0 && morsel.tags[0] !== ""}
+    <div class="flex flex-row py-4 gap-2">
+      {#each morsel.tags as tag}
+        <Badge>{tag}</Badge>
+      {/each}
+    </div>
+    <!-- <p>Tags: {morsel.tags.join(", ")}</p> -->
   {/if}
   <p>{morsel.text}</p>
 </div>
