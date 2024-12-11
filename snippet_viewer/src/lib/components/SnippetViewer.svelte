7e9141d87d00d17bfd150c1dbd7faf0e35550f3d
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-11 14:37:35 -0500

    add a snippet viewer component and display it in the editor page

---
+++ snippet_viewer/src/lib/components/SnippetViewer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let dummySnippetData = $state({
+    uuid: "1234",
+    tags: ["tag1", "tag2"],
+    snippets: [
+      {
+        title: "Title 1",
+        text: "Text 1",
+        tags: ["tag1"],
+        time: "2021-01-01T00:00:00.000Z",
+      },
+      {
+        title: "Title 2",
+        text: "Text 2",
+        tags: ["tag2"],
+        time: "2021-01-02T00:00:00.000Z",
+      },
+    ],
+  });
+</script>
+
+<pre>{JSON.stringify(dummySnippetData, null, 2)}</pre>
--- snippet_viewer/src/routes/editor/+page.svelte
+++ snippet_viewer/src/routes/editor/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold tracking-tight">Coming Soon</h1>
+<script lang="ts">
+  import SnippetViewer from "$lib/components/SnippetViewer.svelte";
+</script>
+
+<div class="flex flex-col gap-4">
+  <h1 class="text-3xl font-bold tracking-tight">Snippet Viewer Data</h1>
+  <SnippetViewer />
+</div>
