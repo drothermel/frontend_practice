5acdb0e81b58394241479b2d049a50f8aa93bc00
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-11 19:28:37 -0500

    made it possible to remove morsels from library

--- snippet_viewer/src/lib/components/MorselCard.svelte
+++ snippet_viewer/src/lib/components/MorselCard.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
-  import { type Morsel } from "$lib/types/types";
+  import { type Morsel, type MorselViewerContext } from "$lib/types/types";
+  import { getMorselViewerContext } from "$lib/components/context";
+  import { SquareX } from "lucide-svelte";
 
   import * as Card from "./ui/card/index";
   import { Badge } from "./ui/badge";
+  import { Button } from "./ui/button";
 
   let { morsel }: { morsel: Morsel } = $props();
+  let morselContext: MorselViewerContext = getMorselViewerContext();
+
+  function onDelete() {
+    console.log("Tried to delete morsel", morsel);
+    morselContext.morsels = morselContext.morsels.filter((m) => m !== morsel);
+    morselContext.tags = Array.from(
+      new Set(morselContext.morsels.flatMap((morsel) => morsel.tags))
+    );
+  }
 </script>
 
-<Card.Root class="w-full">
+<Card.Root class="w-full relative">
+  <Button
+    variant="ghost"
+    size="icon"
+    class="absolute right-2 top-2 h-6 w-6 rounded-full hover:bg-destructive hover:text-destructive-foreground"
+    onclick={() => onDelete()}
+  >
+    <SquareX />
+  </Button>
   <Card.Header>
     <Card.Title tag="h3" class="text-xl font-bold">{morsel.title}</Card.Title>
     <Card.Description class="flex flex-col gap-1">
