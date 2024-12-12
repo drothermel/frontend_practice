8f181aac5bafcf85d30ee8f89fae87fcc01d966f
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-11 21:48:11 -0500

    calling the dummy api and displaying JSONified on the editor page

--- arxiv_viewer/src/routes/editor/+page.svelte
+++ arxiv_viewer/src/routes/editor/+page.svelte
@@ -1,1 +1,29 @@
-<h1 class="text-3xl font-bold tracking-tight">Coming Soon</h1>
+<script lang="ts">
+  type Post = {
+    title: string;
+    body: string;
+  };
+
+  async function getPosts() {
+    const response = await fetch("https://dummyjson.com/posts");
+    const { posts } = await response.json();
+    return posts as Post[];
+  }
+
+  function slugify(text: string) {
+    return text
+      .replace(/\s/g, "-")
+      .replace(/[^a-zA-Z0-9-]/g, "")
+      .toLowerCase();
+  }
+</script>
+
+<h1 class="text-3xl font-bold tracking-tight">Dummy JSON Posts</h1>
+
+{#await getPosts()}
+  <p>Loading...</p>
+{:then posts}
+  <pre>{JSON.stringify(posts, null, 2)}</pre>
+{:catch error}
+  <p>{error.message}</p>
+{/await}
