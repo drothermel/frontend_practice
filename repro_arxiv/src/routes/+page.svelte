0963824257a53c09348f2a6bc8a352507f4ea106
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-17 20:13:33 -0500

    figured out prev issue: explicitly setting type any breaks everything

--- repro_arxiv/src/routes/+page.svelte
+++ repro_arxiv/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let inputText = $state("");
+</script>
+
+<div class="prose p-4 flex flex-col">
+  <h1>Arxiv Query Viewer</h1>
+  <input
+    id="query_author"
+    type="text"
+    placeholder="Author Name"
+    bind:value={inputText}
+    class="border border-green-900"
+  />
+  <button class="border border-green-900 bg-slate-400">Query Arxiv API</button>
+  <h4>Results for query: "{inputText}"</h4>
+  <span>TBD</span>
+</div>
