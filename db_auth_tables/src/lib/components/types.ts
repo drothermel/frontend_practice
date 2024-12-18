25d8f85e5c8f5c8ba730bf2dc64a3afcd352c9a8
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-17 23:19:17 -0500

    fully typed arxiv response, parsed and pretty printed json on relev page

--- db_auth_tables/src/lib/components/types.ts
+++ db_auth_tables/src/lib/components/types.ts
@@ -26,10 +26,89 @@ export type ArxivQueryOperators = 'AND' | 'OR' | 'ANDNOT';
 export type ArxivSortByKeys = 'relevance' | 'lastUpdatedDate' | 'submittedDate';
 export type ArxivSortOrderKeys = 'ascending' | 'descending';
 
-export type ArxivResponse = string;
-export type ArxivResult = any;
+export type ArxivMetadata = {
+    id?: string;
+    updated?: string;
+    published?: string;
+    title?: string;
+    summary?: string;
+    authors?: string[];
+    absLink?: string;
+    pdfLink?: string;
+    primaryCategory?: string;
+    categories?: string[];
+    doi?: string;
+    comment?: string;
+    journal?: string;
+    reportNumber?: string;
+}
 
 export type ArxivQueryAndResult = {
     query: ArxivQueryParams;
-    result: ArxivResult[];
+    result: ArxivMetadata[];
+}
+
+export type ArxivResponseTextField = {
+    _text: string;
+}
+export type ArxivResponseAuthorField = {
+    name: ArxivResponseTextField;
+}
+export type ArxivResponseLinkField = {
+    _attributes: {
+        title?: string;
+        href: string;
+        rel: string;
+        type: string;
+    }
+}
+export type ArxivResponseCategoryField = {
+    _attributes: {
+        term: string;
+        scheme: string;
+        "xmlns:string"?: string;
+    }
+}
+export type ArxivResponseEntry = {
+    id: ArxivResponseTextField;
+    updated: ArxivResponseTextField;
+    published: ArxivResponseTextField;
+    title: ArxivResponseTextField;
+    summary: ArxivResponseTextField;
+    author: ArxivResponseAuthorField[] | ArxivResponseAuthorField;
+    link: ArxivResponseLinkField[];
+    category: ArxivResponseCategoryField[] | ArxivResponseCategoryField;
+    "arxiv:primary_category": ArxivResponseCategoryField;
+}
+type ArxivResponseOpenSearch = {
+    _attributes: {
+        "xmlns:opensearch": string;
+    };
+    _text: string;
+}
+export type ArxivResponse = {
+    feed: {
+        entry: ArxivResponseEntry[];
+    };
+    id: ArxivResponseTextField;
+    link: ArxivResponseLinkField; // query link
+    "opensearch:itemsPerPage": ArxivResponseOpenSearch;
+    "opensearch:startIndex": ArxivResponseOpenSearch;
+    "opensearch:totalResults": ArxivResponseOpenSearch;
+    title: {
+        _attributes: {
+            type: string;
+        };
+        _text: string;
+    };
+    updated: ArxivResponseTextField;
+    _attributes: {
+        xmlns: string;
+    };
+    _declaration: {
+        _attributes: {
+            version: string;
+            encoding: string;
+        }
+    };
 }
