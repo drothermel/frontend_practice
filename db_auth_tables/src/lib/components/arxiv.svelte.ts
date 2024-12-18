29fe91d7cd7a119c87152ab719859cd82fc2edba
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-17 23:19:17 -0500

    fully typed arxiv response, parsed and pretty printed json on relev page

--- db_auth_tables/src/lib/components/arxiv.svelte.ts
+++ db_auth_tables/src/lib/components/arxiv.svelte.ts
@@ -5,42 +5,40 @@ import convert from "xml-js";
 
 import type {
     ArxivQueryAndResult,
-    ArxivQueryParams, ArxivResponse, ArxivResult
+    ArxivQueryParams,
+    ArxivMetadata,
+    ArxivResponse,
+    ArxivResponseEntry,
+    ArxivResponseAuthorField,
+    ArxivResponseLinkField
 } from '$lib/components/types.ts';
 
-
-// TODO: type for result feed!
 export default class ArxivQueryHandler {
     // Default query params, maxResults higher than api default
     queryBase: string = 'https://export.arxiv.org/api/query?';
     currQuery: ArxivQueryParams = $state({
         start: 0,
-        maxResults: 100,
+        maxResults: 2,
         sortBy: 'lastUpdatedDate',
         sortOrder: 'descending',
+        author: "Kyunghyun Cho",
     });
     history: ArxivQueryAndResult[] = $state([]);
-    resultFeed: any[] | null = $state(null);
+    resultFeed: object[] = $state([]);
 
-
-    async queryArxivLoadFeed() {
-        let queryStr = this.buildQuery(this.currQuery);
-        const response = await fetch(queryStr);
-        const textResponse: ArxivResponse = await response.text();
-        this.convertResponseToFeed(textResponse);
+    setQueryParams(params: ArxivQueryParams) {
+        this.currQuery = params;
     }
 
-    convertResponseToFeed(response: ArxivResponse): void {
-        const jsonString: string = convert.xml2json(
-            response, 
-            {
-                compact: true,
-                spaces: 2,
-            }
-        );
-        const jsonObject = JSON.parse(jsonString);
-        const entries = jsonObject["feed"]["entry"] || {};
-        this.resultFeed = entries;
+    async queryArxivLoadFeed() {
+        let queryStr: string = this.buildQuery(this.currQuery);
+        console.log(queryStr);
+        const response: Response = await fetch(queryStr);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const textResponse: string = await response.text();
+        this.convertResponseTextToJson(textResponse);
     }
 
     buildQuery(params: ArxivQueryParams): string {
@@ -48,14 +46,15 @@ export default class ArxivQueryHandler {
         if (params.ids) {
             query += 'id_list=' + params.ids.join(',') + '&';
         } else {
-            if (params.title) query += 'title=' + params.title + '&';
-            if (params.author) query += 'author=' + params.author + '&';
-            if (params.abstract) query += 'abstract=' + params.abstract + '&';
-            if (params.comment) query += 'comment=' + params.comment + '&';
-            if (params.journal) query += 'journal_ref=' + params.journal + '&';
-            if (params.category) query += 'cat=' + params.category + '&';
-            if (params.reportNumber) query += 'report_num=' + params.reportNumber + '&';
-            if (params.all) query += 'all=' + params.all + '&';
+            query += 'search_query=';
+            if (params.title) query += 'ti:' + params.title + '&';
+            if (params.author) query += 'au:"' + params.author + '"&';
+            if (params.abstract) query += 'abs:' + params.abstract + '&';
+            if (params.comment) query += 'co:' + params.comment + '&';
+            if (params.journal) query += 'jr:' + params.journal + '&';
+            if (params.category) query += 'cat:' + params.category + '&';
+            if (params.reportNumber) query += 'rn:' + params.reportNumber + '&';
+            if (params.all) query += 'all:' + params.all + '&';
         }
         if (params.start) query += 'start=' + params.start + '&';
         if (params.maxResults) query += 'max_results=' + params.maxResults + '&';
@@ -66,20 +65,53 @@ export default class ArxivQueryHandler {
         return query;
     }
 
-    setQueryParams(params: ArxivQueryParams) {
-        this.currQuery = params;
+    convertResponseTextToJson(response: string) {
+        const jsonString: string = convert.xml2json(
+            response, 
+            {
+                compact: true,
+                spaces: 2,
+            }
+        );
+        const result: ArxivResponse = JSON.parse(jsonString);
+        const entries: ArxivResponseEntry[] = result['feed']['entry'];
+        this.resultFeed = entries.map((e) => {return this.parseArxivFeedEntry(e)});
     }
 
-    // TODO: We need a type for authors
-    authorsToString(authors: any[], query: ArxivQueryParams): string {
-        let authorStrings: string[] = [];
-        for (const aData of authors) {
-            const aName = aData.name._text;
-            if (query.author && aName === query.author){
-                authorStrings.push(`<span class="font-bold">${aName}</span>`);
-            }
-            authorStrings.push(aName);
+    parseArxivFeedEntry(entry: ArxivResponseEntry): ArxivMetadata {
+        let metadata: ArxivMetadata =  {
+            id: entry.id._text,
+            updated: entry.updated._text,
+            published: entry.published._text,
+            title: entry.title._text.replace(/\r?\n/g, " "),
+            summary: entry.summary._text.replace(/\r?\n/g, " "),
+            authors: Array.isArray(entry.author) ? entry.author.map(
+                (a: ArxivResponseAuthorField) => { return a.name._text; }
+            ) : [entry.author.name._text],
+            primaryCategory: entry["arxiv:primary_category"]._attributes.term,
+            categories: Array.isArray(entry.category) ? entry.category.map(
+                (c) => { return c._attributes.term; }) : [entry.category._attributes.term],
+        };
+
+        let namedLinks: ArxivResponseLinkField[] = entry.link.filter(
+            (l: ArxivResponseLinkField) => {("title" in l)}
+        );
+        let unnamedLinks: ArxivResponseLinkField[] = entry.link.filter(
+            (l: ArxivResponseLinkField) => {!("title" in l)}
+        );
+        if (namedLinks.length > 0 && "_attributes" in namedLinks[0]) {
+            metadata.pdfLink = namedLinks[0]._attributes.href;
         }
+        if (unnamedLinks.length > 0 && "_attributes" in unnamedLinks[0]) {
+            metadata.absLink = unnamedLinks[0]._attributes.href;
+        }
+        return metadata;
+    }
+
+    authorsToString(authors: string[], query: ArxivQueryParams): string {
+        let authorStrings = authors.map((a) => {
+            return a == query.author ? `<span class="font-bold">${a}</span>` : a
+        });
         return authorStrings.join(", ");
     }
 
