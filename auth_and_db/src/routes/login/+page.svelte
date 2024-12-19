f3922167e413e55dc07b5d2e40fc1904cccf2096
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-19 04:27:02 -0500

    add login route

---
+++ auth_and_db/src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let { form } = $props();
+</script>
+
+<h1>Login</h1>
+
+<form action="?/login" method="POST">
+  <div>
+    <label for="username">Username</label>
+    <input id="username" name="username" type="text" required />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input id="password" name="password" type="password" required />
+  </div>
+
+  {#if form?.invalid}
+    <p class="error">Username and password are both required.</p>
+  {/if}
+  {#if form?.credentials}
+    <p class="error">You have entered the wrong credentials</p>
+  {/if}
+  <button type="submit">Login</button>
+</form>
