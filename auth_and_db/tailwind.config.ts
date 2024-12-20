ffa5398e1183036800def047773b09a1033642c2
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-19 19:09:08 -0500

    addtypeography plugin to tailwind config

--- auth_and_db/tailwind.config.ts
+++ auth_and_db/tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography';
 
 const config: Config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  plugins: [typography],
 }
 
 export default config;
