02e722261cbd8fe327fb524feaa2959dec72f7d4
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-19 19:35:52 -0500

    add daisyui emerald theme, apply, use colors for body background and nav text

--- auth_and_db/tailwind.config.ts
+++ auth_and_db/tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 const config: Config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  plugins: [typography],
+  plugins: [typography, daisyui],
+  daisyui: {
+    themes: ["emerald"],
+    darkTheme: "emerald",
+    base: true, // Include DaisyUI's base styles
+    styled: true, // Include DaisyUI's component styling
+    utils: true, // Enable DaisyUI's utilities
+    logs: false, // Show logs in the console
+    rtl: false, // Support for right-to-left layouts
+    prefix:"", // DaisyUI class name prefix (if needed)
+  },
 }
 
 export default config;
