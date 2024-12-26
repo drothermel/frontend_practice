5ae9c8b8691464dad7222ae2510972584d05506e
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-26 09:52:11 -0500

    add sidebar panel from shadcn

---
+++ snippet_viewer_v1/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,35 @@
+<script>
+  import House from "lucide-svelte/icons/house";
+  import * as Sidebar from "$lib/components/ui/sidebar/index";
+  const menu_items = [
+    {
+      icon: House,
+      title: "Home",
+      url: "/",
+    },
+  ];
+</script>
+
+<Sidebar.Root>
+  <Sidebar.Content>
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>ByTomorrow App</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          {#each menu_items as item (item.title)}
+            <Sidebar.MenuItem>
+              <Sidebar.MenuButton>
+                {#snippet child({ props })}
+                  <a href={item.url} {...props}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                {/snippet}
+              </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+          {/each}
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+  </Sidebar.Content>
+</Sidebar.Root>
